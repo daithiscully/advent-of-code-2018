a398370cc3063a15c96de5e488900170fe47895b
david <david@expertsoftwaretest.ie>
2018-12-03 20:30:14 +0000

    Day 2: Part two WIP

--- src/main/java/com/scully/daytwo/Helper.java
+++ src/main/java/com/scully/daytwo/Helper.java
@@ -13,13 +13,13 @@ public class Helper {
 
 
   public static List<String> getPuzzleInput() throws URISyntaxException, IOException {
-    URL resource = DayOneMain.class.getResource("/day-two/input.txt");
+    URL resource = Helper.class.getResource("/day-two/input.txt");
     Path path = Paths.get(resource.toURI());
     return Files.readAllLines(path);
   }
 
   public static List<String> getSampleInput() throws URISyntaxException, IOException {
-    URL resource = DayOneMain.class.getResource("/day-two/sample.txt");
+    URL resource = Helper.class.getResource("/day-two/sample.txt");
     Path path = Paths.get(resource.toURI());
     return Files.readAllLines(path);
   }
--- src/main/java/com/scully/daytwo/PartTwo.java
+++ src/main/java/com/scully/daytwo/PartTwo.java
@@ -1,17 +1,8 @@
 package com.scully.daytwo;
 
-import com.google.common.collect.Maps;
-import com.scully.dayone.DayOneMain;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -20,8 +11,45 @@ public class PartTwo {
   public static void main(String[] args) throws URISyntaxException, IOException {
     log.info("Advent code Day two: Part two");
     // Puzzle Input
-    List<String> boxIDs = Helper.getPuzzleInput();
+//    List<String> boxIDs = Helper.getPuzzleInput();
+    List<String> boxIDs = Helper.getSampleInput();
+
+    log.info("Box IDs: {}", boxIDs.toString());
 
 
+    for (int i = 0; i < boxIDs.size(); i++) {
+      String currentBoxID = boxIDs.get(i);
+      boolean foundPartner = true;
+
+
+      char[] currentBoxIDChars = currentBoxID.toCharArray();
+
+
+      for (int j = 0; j < boxIDs.size(); j++) {
+        String otherBoxID = boxIDs.get(j);
+        if (currentBoxID.equals(otherBoxID)){
+
+        } else {
+          char[] otherBoxIDChars = otherBoxID.toCharArray();
+          for (int k = 0; k < currentBoxIDChars.length; k++) {
+            int numberOfMisses = 0;
+// currentBoxID: fghij otherBoxID: fguij -> will partner work
+            if (currentBoxIDChars[k] == otherBoxIDChars[k]){
+              log.info("currentBoxID: {} otherBoxID: {}", currentBoxID,  otherBoxID);
+              log.info("currentBoxIDChars[k] == otherBoxIDChars[k]: {}", currentBoxIDChars[k] == otherBoxIDChars[k] );
+            } else {
+              numberOfMisses++;
+            }
+            if (numberOfMisses >= 1) {
+              foundPartner = false;
+              log.info("Pair: {} & {} failed to partner", currentBoxID, otherBoxID);
+              break;
+            } else {
+            }
+          }
+        }
+      }
+      log.info("Found Partner? {}", foundPartner);
+    }
   }
 }
