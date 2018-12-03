d49756ac115216f1b3c1c193f8039c9207d61805
david <david@expertsoftwaretest.ie>
2018-12-03 21:56:57 +0000

    Day 2: Part two done

--- src/main/java/com/scully/daytwo/PartTwo.java
+++ src/main/java/com/scully/daytwo/PartTwo.java
@@ -1,11 +1,14 @@
 package com.scully.daytwo;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+@Slf4j(topic = "->")
 public class PartTwo {
 
   public static void main(String[] args) throws URISyntaxException, IOException {
@@ -16,40 +19,38 @@ public class PartTwo {
 
     log.info("Box IDs: {}", boxIDs.toString());
 
+    Set<String> partners = Sets.newHashSet();
 
+    int numberOfPartners = 0;
     for (int i = 0; i < boxIDs.size(); i++) {
       String currentBoxID = boxIDs.get(i);
-      boolean foundPartner = true;
-
-
       char[] currentBoxIDChars = currentBoxID.toCharArray();
-
-
       for (int j = 0; j < boxIDs.size(); j++) {
         String otherBoxID = boxIDs.get(j);
         if (currentBoxID.equals(otherBoxID)){
 
         } else {
           char[] otherBoxIDChars = otherBoxID.toCharArray();
+          int numberOfMisses = 0;
           for (int k = 0; k < currentBoxIDChars.length; k++) {
-            int numberOfMisses = 0;
-// currentBoxID: fghij otherBoxID: fguij -> will partner work
             if (currentBoxIDChars[k] == otherBoxIDChars[k]){
-              log.info("currentBoxID: {} otherBoxID: {}", currentBoxID,  otherBoxID);
-              log.info("currentBoxIDChars[k] == otherBoxIDChars[k]: {}", currentBoxIDChars[k] == otherBoxIDChars[k] );
+              log.info("Chars: {}-{}", currentBoxIDChars[k], otherBoxIDChars[k]);
             } else {
               numberOfMisses++;
             }
-            if (numberOfMisses >= 1) {
-              foundPartner = false;
-              log.info("Pair: {} & {} failed to partner", currentBoxID, otherBoxID);
-              break;
-            } else {
-            }
+          }
+          if (numberOfMisses >= 2) {
+            log.info("Pair: {} & {} failed to partner", currentBoxID, otherBoxID);
+          } else {
+            numberOfPartners++;
+            log.info("Pair: {} found it's partner: {} ", currentBoxID, otherBoxID);
+            partners.add(currentBoxID);
+            partners.add(otherBoxID);
           }
         }
       }
-      log.info("Found Partner? {}", foundPartner);
     }
+    log.info("Number of partners: {}", numberOfPartners);
+    partners.forEach(log::info);
   }
 }
