98f059e6434040cf5d41a3f71f208c5e4074f4ca
david <david@expertsoftwaretest.ie>
2018-12-04 10:44:28 +0000

    Day 3: Part One - started the use of a 2d array (matrix)

--- src/main/java/com/scully/daythree/PartOne.java
+++ src/main/java/com/scully/daythree/PartOne.java
@@ -1,5 +1,6 @@
 package com.scully.daythree;
 
+import com.google.common.collect.Lists;
 import com.scully.Helper;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -10,44 +11,48 @@ import lombok.extern.slf4j.Slf4j;
 public class PartOne {
 
   public static void main(String[] args) throws IOException, URISyntaxException {
-    // at least 1000 inches on each side
-
+    log.info("Advent code Day three: Part one");
+    //    List<String> claimsRaw = Helper.getPuzzleInput("day-three");
+    List<String> claimsRaw = Helper.getSampleInput("day-three");
     // cID   L T  w h
     //#123 @ 3,2: 5x4
     //#1 @ 1,3: 4x4
     //#2 @ 3,1: 4x4
     //#3 @ 5,5: 2x2
-    Claim claim = new Claim("#1", 1, 3, 4, 4);
 
-//    List<String> claimsRaw = Helper.getPuzzleInput("day-three");
-    List<String> claimsRaw = Helper.getSampleInput("day-three");
+    // increment the matrix location based on each claims co-ordinates
+    int[][] fabricMatrix = new int[5][5];
+    List<Claim> claims = getClaims(claimsRaw);
+    log.info("Claims: {}", claims);
+    claims.forEach(claim -> {
+//      fabricMatrix[0][0] = 1;
+//      log.info("Fabric Matrix: {}", fabricMatrix[0][0]);
+    });
+
+  }
+
+  private static List<Claim> getClaims(List<String> claimsRaw) {
+    List<Claim> claims = Lists.newArrayList();
 
     claimsRaw.forEach(rawClaim -> {
       String id = rawClaim.split(" ")[0];
-      String fromLeft = rawClaim.split("")[0];
-//      String fromTop = rawClaim.split(",", 1)[1];
-//      String width = rawClaim.split(" ")[3];
-//      String height = rawClaim.split(" ")[4];
+      CharSequence fromLeftChar = rawClaim
+          .subSequence(rawClaim.indexOf("@") + 1, rawClaim.indexOf(","));
+      String fromLeft = fromLeftChar.toString().trim();
+      CharSequence fromTopChar = rawClaim
+          .subSequence(rawClaim.indexOf(",") + 1, rawClaim.indexOf(":"));
+      String fromTop = fromTopChar.toString().trim();
+      CharSequence widthChar = rawClaim
+          .subSequence(rawClaim.indexOf(":") + 1, rawClaim.indexOf("x"));
+      String width = widthChar.toString().trim();
+      CharSequence heightChar = rawClaim.subSequence(rawClaim.indexOf("x") + 1, rawClaim.length());
+      String height = heightChar.toString().trim();
 
-//      log.info("{}", id);
-      log.info("{}", fromLeft);
-//      log.info("{}, {}, {}, {}, {}", id, fromLeft, fromTop, width, height);
-
-
-
-//      int fromLeft = Integer.parseInt(rawClaim.split(" ")[2]);
-//      int fromTop = Integer.parseInt(rawClaim.split(",", 1)[1]);
-//      int width = Integer.parseInt(rawClaim.split(" ")[3]);
-//      int height = Integer.parseInt(rawClaim.split(" ")[4]);
-
-//      Claim claim2 = new Claim(id, fromLeft, fromTop, width, height);
-
-//      log.info("{}", claim2);
+      Claim claim = new Claim(id, Integer.parseInt(fromLeft), Integer.parseInt(fromTop),
+          Integer.parseInt(width), Integer.parseInt(height));
+      claims.add(claim);
     });
-
-    log.info("Claims: {}", claimsRaw);
-
-
+    return claims;
   }
 
 
